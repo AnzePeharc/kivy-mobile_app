9dc22438cf439e0cb2e30e4ef9f413c2cca54cf7
anzepeharc <anze.peharc@gmail.com>
2020-06-15 18:00:26 +0200

    MVP client and server for RPI WiFi communication

--- client.py
+++ client.py
@@ -1,11 +1,14 @@
 import socket
 
-host = '127.0.0.1'  # The server's hostname or IP address
-port = 8888  # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((host, port))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
+host = '172.20.10.3'  # The server's hostname or IP address
+port = 12345  # The port used by the server
 
-print('Received', repr(data))
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((host, port))
+client.send("I am CLIENT<br>".encode("utf-8"))
+from_server = client.recv(4096)
+client.close()
+print(from_server)
+
+
--- server.py
+++ server.py
@@ -1,17 +1,19 @@
 import socket
 
-host = '127.0.0.1'  # Standard loopback interface address (localhost)
-port = 8888  # Port to listen on (non-privileged ports are > 1023)
-print("Listening to incoming messages:")
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((host, port))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            print(data)
-            if not data:
-                break
-            conn.sendall(data)
+host = "172.20.10.3"
+port = 12345
+serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serv.bind((host, port))
+serv.listen(5)
+while True:
+    conn, addr = serv.accept()
+    from_client = ''
+    while True:
+        data = conn.recv(4096)
+        if not data:
+            break
+        from_client += data.decode("utf-8")
+        print(from_client)
+        conn.send("I am SERVER<br>".encode("utf-8"))
+    conn.close()
+    print("Connection closed!")
