24a8e6ebd5ac2d2fb56b4a12aa34ae0d48c7f173
anzepeharc <anze.peharc@gmail.com>
2020-09-15 12:50:17 +0200

    Server working on RPI

--- client.py
+++ client.py
@@ -2,16 +2,23 @@ import socket
 
 from distlib.compat import raw_input
 
-host = 'localhost'  # '172.20.10.3'  # The server's hostname or IP address
-port = 12345  # The port used by the server
+host = '172.20.10.3'  # 'localhost' # The server's hostname or IP address
+port = 80  # The port used by the server
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((host, port))
+#client.settimeout(5.0)
+try:
+    client.connect((host, port))
+except socket.error as e:
+    print('Error: ' + str(e))
+    client.close()
+
+
 while True:
     str = raw_input("S: ")
     client.send(str.encode())
     if str == "Bye" or str == "bye":
         break
-    print("N: " + client.recv(1024).decode())
+    #print("N: " + client.recv(1024).decode())
 
 client.close()
